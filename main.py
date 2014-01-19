a0680f3b8c9774d50ea07c615fa8281b8ff89124
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2014-01-19 12:26:59 +0700

    kv

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.graphics import Color, Ellipse, Line
-from random import random
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty, StringProperty
 
-class MyPaintWidget(Widget):
-    def on_touch_down(self, touch):
-    	color = (random(), random(), random())
-        with self.canvas:
-            Color(*color)
-            d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
+class Controller(FloatLayout):
+	label_wid = ObjectProperty()
+	info = StringProperty()
 
-    def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
+	def do_action(self):    	
+		self.label_wid.text = 'My label after button press'
+		self.info = 'New info text'    
 
-class MyPaintApp(App):
+class LetterOfHeroesApp(App):
 	def build(self):
-		parent = Widget()
-		painter = MyPaintWidget()
-		clearbtn = Button(text='Clear')
-		parent.add_widget(painter)
-		parent.add_widget(clearbtn)
-
-		def clear_canvas(obj):
-			painter.canvas.clear()
-
-		clearbtn.bind(on_release=clear_canvas)
-		return parent
-
-
+		return Controller(info="Letter of Heroes")
 
 if __name__ == '__main__':
-    MyPaintApp().run()
+    LetterOfHeroesApp().run()
